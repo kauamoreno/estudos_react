c68c84af310848f371ee454662c1bce1f19b5c0b
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-02-18 00:39:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const myName = "Kauã"
+
+  function soma(x, y){
+    return x+y
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu app React</h1>
+      <p>lorem ipsum dorem</p>
+      <p>O meu nome é {myName}</p>
+      <p>A soma: {soma(2, 3)}</p>      
+      <img src={url} alt='sdfsdf'/>
     </div>
   );
 }
