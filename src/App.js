9095ad793555fe118760bd62ec7dd6b94ba56355
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-02-18 13:59:45 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWord from './components/HelloWord';
 
 function App() {
 
   const myName = "Kauã"
 
-  function soma(x, y){
-    return x+y
+  function soma(x, y) {
+    return x + y
   }
 
   const url = 'https://via.placeholder.com/150'
@@ -16,8 +16,11 @@ function App() {
       <h1>Meu app React</h1>
       <p>lorem ipsum dorem</p>
       <p>O meu nome é {myName}</p>
-      <p>A soma: {soma(2, 3)}</p>      
-      <img src={url} alt='sdfsdf'/>
+      <p>A soma: {soma(2, 3)}</p>
+      <img src={url} alt='sdfsdf' />
+
+      <HelloWord />
+
     </div>
   );
 }
---
+++ src/components/HelloWord.js
@@ -0,0 +1,13 @@
+import Frase from './Frase'
+
+function HelloWord(){
+
+    return(
+        <div>
+            <h1>Meu primeiro componente</h1>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWord;
